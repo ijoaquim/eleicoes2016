ec7b800df3dca280d27994251b2f9d0387d7b526
ijoaquim <itamarjoaquim@yahoo.com.br>
2018-08-20 10:32:28 -0300

    Add files via upload

---
+++ 2018/divulgacao/homologacaotre/7666/config/ac/ac-e007666-mw.js
@@ -0,0 +1,4 @@
+
+{ "codEle" : "7666", "datGer" : "09/08/2018", "horGer" : "13:03:46", "f" : "S", "cod" : "AC", "des" : "ACRE", "tip" : "UF", "municipioDominio" : [ 
+{ "cod" : "01570", "codIbge" : "1200054", "nom" : "ASSIS BRASIL", "cap" : "N", "zon" : [ "0006" ] }, 
+{ "cod" : "01392", "codIbge" : "1200401", "nom" : "RIO BRANCO", "cap" : "S", "zon" : [ "0010", "0009", "0001" ] } ] }
